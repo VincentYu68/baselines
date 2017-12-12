d420aadec09b6867e6a1b9a7137ba610a3bdf953
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2017-12-12 11:34:03 -0500

    bug fix

--- baselines/ppo1/run_walker3d_learning.py
+++ baselines/ppo1/run_walker3d_learning.py
@@ -58,7 +58,7 @@ def train_mirror(env_id, num_timesteps, seed):
                                                       -9, -10, 11, 12, -13,
                                                       20, 21, -22, 23, -24, -25, -26, -27, 28, 35, -36, -37, 38, 39,
                                                       -40, 29, -30, -31, 32, 33,
-                                                      -34, 42, 41]),
+                                                      -34, 42, 41, 43]),
                                                  action_permutation=np.array(
                                                      [-0.0001, -1, 2, 9, -10, -11, 12, 13, -14, 3, -4, -5, 6, 7, -8]))
     env = bench.Monitor(env, logger.get_dir() and

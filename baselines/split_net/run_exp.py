a003ebd81ee01acf3ed92de1f766be5e1147d1b5
Wenhao Yu <wenhaoyu@Wenhaos-MacBook-Pro.local>
2018-02-05 21:10:10 -0500

    improved automation

--- baselines/split_net/run_exp.py
+++ baselines/split_net/run_exp.py
@@ -25,7 +25,7 @@ def callback(localv, globalv):
         joblib.dump(save_dict, logger.get_dir() + '/policy_params_t'+str(t) + '.pkl', compress=True)
 
 
-def train(env_id, num_timesteps, seed, split_iter, split_percent):
+def train(env_id, num_timesteps, batch, seed, split_iter, split_percent):
     from baselines.split_net import mlp_split_policy, pposgd_split
     U.make_session(num_cpu=1).__enter__()
     set_global_seeds(seed)
@@ -39,7 +39,7 @@ def train(env_id, num_timesteps, seed, split_iter, split_percent):
     gym.logger.setLevel(logging.WARN)
     pposgd_split.learn(env, policy_fn,
             max_timesteps=num_timesteps,
-            timesteps_per_batch=int(1000),
+            timesteps_per_batch=int(batch),
             clip_param=0.2, entcoeff=0.0,
             optim_epochs=10, optim_stepsize=3e-4, optim_batchsize=64,
             gamma=0.99, lam=0.95, schedule='linear',
@@ -56,10 +56,11 @@ def main():
     parser.add_argument('--seed', help='RNG seed', type=int, default=0)
     parser.add_argument('--split_iter', help='iteration number that starts splitting', type=int, default=0)
     parser.add_argument('--split_percent', help='number of splitted parameters', type=float, default=0.0)
+    parser.add_argument('--batch', help='batch per thread', type=int, default=1000)
     args = parser.parse_args()
     logger.reset()
-    logger.configure('data/ppo_'+args.env+str(args.seed)+'_split_'+str(args.split_iter)+'_'+str(args.split_percent)+'_2task_forwardbackward_8k')
-    train(args.env, num_timesteps=int(2000*4*300), seed=args.seed, split_iter=args.split_iter, split_percent=args.split_percent)
+    logger.configure('data/ppo_'+args.env+str(args.seed)+'_split_'+str(args.split_iter)+'_'+str(args.split_percent)+'_2task_forwardbackward_'+str(args.batch))
+    train(args.env, num_timesteps=int(2000*4*300), batch = args.batch, seed=args.seed, split_iter=args.split_iter, split_percent=args.split_percent)
 
 if __name__ == '__main__':
     main()
